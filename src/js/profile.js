edb7a13a4e57ef5244a7281b1df55f2287310e7e
Alexander Kuzmin <roosit@abricos.org>
2015-09-11 16:29:26 +0300

    draft: profile info widget

--- src/js/profile.js
+++ src/js/profile.js
@@ -10,25 +10,58 @@ Component.entryPoint = function(NS){
         COMPONENT = this,
         SYS = Brick.mod.sys;
 
-    NS.AvatarWidget = Y.Base.create('avatarWidget', SYS.AppWidget, [], {
+    var ProfileWidgetExt = function(){
+    };
+    ProfileWidgetExt.prototype = {
         onInitAppWidget: function(err, appInstance){
-            this._idWidget = NS.AvatarWidget._counter++;
-            NS.AvatarWidget._activeWidgets[this._idWidget] = this;
-
-            this.set('waiting', true);
-
             if (this.get('userid') === 0){
                 this.set('userid', Brick.env.user.id);
             }
             var userid = this.get('userid');
+
+            this.set('waiting', true);
             appInstance.profile(userid, function(err, result){
-                this.renderAvatar();
+                this.set('waiting', false);
+                this.onInitProfileWidget(err, appInstance);
+                if (result.profile){
+                    this.renderProfile();
+                }
             }, this);
+        },
+        onInitProfileWidget: function(){
+        },
+        renderProfile: function(){
+        }
+    };
+    ProfileWidgetExt.NAME = 'profileWidgetExt';
+    ProfileWidgetExt.ATTRS = {
+        profile: {
+            readOnly: true,
+            getter: function(){
+                var app = this.get('appInstance');
+                if (!app){
+                    return null;
+                }
+                return app.get('profileList').getById(this.get('userid'));
+            }
+        },
+        userid: {
+            validator: Y.Lang.isNumber
+        },
+    };
+    NS.ProfileWidgetExt = ProfileWidgetExt;
+
+    NS.AvatarWidget = Y.Base.create('avatarWidget', SYS.AppWidget, [
+        NS.ProfileWidgetExt
+    ], {
+        onInitProfileWidget: function(err, appInstance){
+            this._idWidget = NS.AvatarWidget._counter++;
+            NS.AvatarWidget._activeWidgets[this._idWidget] = this;
         },
         destructor: function(){
             delete NS.AvatarWidget._activeWidgets[this._idWidget];
         },
-        renderAvatar: function(){
+        renderProfile: function(){
             this.set('waiting', false);
             var profile = this.get('profile');
             if (!profile){
@@ -69,19 +102,6 @@ Component.entryPoint = function(NS){
         ATTRS: {
             component: {value: COMPONENT},
             templateBlockName: {value: 'avatar'},
-            profile: {
-                readOnly: true,
-                getter: function(){
-                    var app = this.get('appInstance');
-                    if (!app){
-                        return null;
-                    }
-                    return app.get('profileList').getById(this.get('userid'));
-                }
-            },
-            userid: {
-                validator: Y.Lang.isNumber
-            },
             avatarSize: {
                 value: 180
             }
@@ -102,21 +122,53 @@ Component.entryPoint = function(NS){
         w.setAvatar(avatar);
     };
 
-    NS.ProfileWidget = Y.Base.create('profileWidget', SYS.AppWidget, [], {
-        onInitAppWidget: function(err, appInstance){
-            this.set('waiting', true);
+    NS.InfoWidget = Y.Base.create('infoWidget', SYS.AppWidget, [
+        NS.ProfileWidgetExt
+    ], {
+        onInitProfileWidget: function(err, appInstance){
+        },
+        destructor: function(){
+        },
+        renderProfile: function(){
+            var tp = this.template,
+                profile = this.get('profile');
 
-            if (this.get('userid') === 0){
-                this.set('userid', Brick.env.user.id);
+            tp.setHTML({
+                username: profile.get('username'),
+                email: profile.get('email')
+            });
+
+            var rows = 'email'.split(','),
+                name, value;
+
+            for (var i = 0; i < rows.length; i++){
+                name = rows[i];
+                value = profile.get(name);
+                tp.toggleView(!!value, name + 'Row');
             }
-            var userid = this.get('userid');
+        }
+    }, {
+        ATTRS: {
+            component: {value: COMPONENT},
+            templateBlockName: {value: 'info'},
+        }
+    });
 
-            appInstance.profile(userid, function(err, result){
-                if (!err){
-                    this.set('profile', result.profile);
-                }
-                this.onLoadProfile();
-            }, this);
+    NS.ProfileWidget = Y.Base.create('profileWidget', SYS.AppWidget, [
+        NS.ProfileWidgetExt
+    ], {
+        onInitProfileWidget: function(){
+            var tp = this.template,
+                userid = this.get('userid');
+
+            this.avatarWidget = new NS.AvatarWidget({
+                srcNode: tp.one('avatar'),
+                userid: userid
+            });
+            this.infoWidget = new NS.InfoWidget({
+                srcNode: tp.one('info'),
+                userid: userid
+            });
         },
         destructor: function(){
             if (this.avatarWidget){
@@ -124,19 +176,8 @@ Component.entryPoint = function(NS){
                 this.avatarWidget = null;
             }
         },
-        onLoadProfile: function(){
-            var tp = this.template;
-            this.avatarWidget = new NS.AvatarWidget({
-                srcNode: tp.one('avatar'),
-                userid: this.get('userid')
-            });
-
-        },
         renderProfile: function(){
             var profile = this.get('profile');
-            if (!profile){
-                return;
-            }
 
             console.log(profile.toJSON());
         }
@@ -144,10 +185,6 @@ Component.entryPoint = function(NS){
         ATTRS: {
             component: {value: COMPONENT},
             templateBlockName: {value: 'widget'},
-            profile: {},
-            userid: {
-                validator: Y.Lang.isNumber
-            }
         }
     });
 
